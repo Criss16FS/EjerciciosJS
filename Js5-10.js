b245738f871548391ada0b4ec7f4aa938b30b24b
Cristian Avila <113220675+Criss16FS@users.noreply.github.com>
2024-04-29 18:19:26 -0500

    Add files via upload

--- Js5-10.js
+++ Js5-10.js
@@ -2,9 +2,9 @@
 
 function areaCuadrado(lado1, lado2) {
     areaCuadrado = lado1 * lado2;
-    console.log(`El area de un cuadrado con medidas de ${lado1} mt y ${lado2} mt es igual a ${areaCuadrado} mt^2 `);
+    console.log(`El area de un cuadrado con medidas de ${lado1} y ${lado2} es igual a ${areaCuadrado} ^2`);
 }
-areaCuadrado(10, 15);
+areaCuadrado(10, 15); // Valor del Lado 1 y Lado 2
 
 
 // Escribe una función que devuelva el área de un círculo dado su radio.
@@ -14,19 +14,35 @@ function areaCirculo(radio, pi){
     areaCirculo = radio**2 * pi;
     console.log(`El area de un circulo con un diametro de ${diametro} mt y un radio de ${radio} mt es igual a ${areaCirculo}  `);
 }
-
-areaCirculo(15 ,Math.PI);
+areaCirculo(15 ,Math.PI);// Valor del radio y Valor de PI
 
 
 // Escribe una función que devuelva el área de un triángulo dado su base y altura.
 
 function areaTriangulo (base, altura){
-
+areaTriangulo = (base * altura) / 2;
+console.log(`El area de un triangulo que tiene una base de ${base} y una altura de ${altura} es igual a ${areaTriangulo}`);
 }
-
-
+areaTriangulo(12, 15); // Valor de la base y la altura
 
 
 // Escribe una función que devuelva el área de un rectángulo dado su base y altura.
+
+function areaRectangulo (base, altura){
+    areaRectangulo = base * altura;
+    console.log(`EL area de un rectangulo el cual tiene uina base de ${base} y una altura de ${altura} es igual a ${areaRectangulo} ^2`);
+
+}
+areaRectangulo(12, 18);// Valor de la base y la altura
+
+
 // Escribe una función que devuelva el área de un trapecio dado sus bases y altura.
+
+function areaTrapecio (base1, base2, altura){
+    areaTrapecio = ((base1+ base2)/2) * altura;
+    console.log(`El area de un trapecio con una base menor de ${base1}, una base mayor de ${base2} y una altura de ${altura} es igual a ${areaTrapecio} ^2`);
+}
+areaTrapecio(3.5 , 9.5, 4);// Valor de la base menor, base mayor y la altura
+
+
 // Escribe una función que devuelva la longitud de la hipotenusa de un triángulo rectángulo dado sus catetos.
