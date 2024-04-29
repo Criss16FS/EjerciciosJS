33663ca8936e5ba6c033fd5b489d80219f433bd8
Cristian Avila <113220675+Criss16FS@users.noreply.github.com>
2024-04-29 17:40:53 -0500

    Add files via upload

---
+++ Js5-10.js
@@ -0,0 +1,32 @@
+// Escribe una función que devuelva el área de un cuadrado dado su lado.
+
+function areaCuadrado(lado1, lado2) {
+    areaCuadrado = lado1 * lado2;
+    console.log(`El area de un cuadrado con medidas de ${lado1} mt y ${lado2} mt es igual a ${areaCuadrado} mt^2 `);
+}
+areaCuadrado(10, 15);
+
+
+// Escribe una función que devuelva el área de un círculo dado su radio.
+
+function areaCirculo(radio, pi){
+    let diametro = radio * 2;
+    areaCirculo = radio**2 * pi;
+    console.log(`El area de un circulo con un diametro de ${diametro} mt y un radio de ${radio} mt es igual a ${areaCirculo}  `);
+}
+
+areaCirculo(15 ,Math.PI);
+
+
+// Escribe una función que devuelva el área de un triángulo dado su base y altura.
+
+function areaTriangulo (base, altura){
+
+}
+
+
+
+
+// Escribe una función que devuelva el área de un rectángulo dado su base y altura.
+// Escribe una función que devuelva el área de un trapecio dado sus bases y altura.
+// Escribe una función que devuelva la longitud de la hipotenusa de un triángulo rectángulo dado sus catetos.
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EjerciciosJS
+Repositorio de ejercicios para practicar lógica de programación usando JS
