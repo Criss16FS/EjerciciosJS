36ec6cc1a03818c9a0bcaeffa0b00d25d3e89dfa
Cristian Avila <113220675+Criss16FS@users.noreply.github.com>
2024-05-06 10:46:44 -0500

    V2

--- Js31-36.js
+++ Js31-36.js
@@ -15,6 +15,21 @@ console.log(sumaDigitos2(22)); // Esto devolverá 10 (6 + 4)
 console.log("La suma es de 22 el igual a 8 (linea superior)");
 
 // Escribe una función que determine si una cadena dada es un pangrama (contiene todas las letras del alfabeto al menos una vez).
+function esPangrama(cadena) {
+    cadena = cadena.toLowerCase();
+    const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+    for (let letra of alfabeto) {
+        if (cadena.indexOf(letra) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Ejemplo de uso:
+console.log(esPangrama("El veloz murciélago hindú comía feliz cardillo y kiwi.")); // Devuelve true
+
+
 // Escribe una función que devuelva el número de veces que aparece una palabra específica en una cadena dada.
 // Escribe una función que convierta un número decimal en su equivalente binario.
 // Escribe una función que devuelva la longitud de la cadena más larga en un arreglo de cadenas dado.
